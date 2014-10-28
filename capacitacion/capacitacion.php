e91705c58b98f75871a6de74332aea8c3fdf6e4b
JuanAlfredo <JuanAlfredo@Kayo.lan>
2014-10-27 22:01:02 -0600

    compilacion final

---
+++ capacitacion/capacitacion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>CAPACITACIÓN</title>
+        <link href="../css/movil.css" rel="stylesheet" type="text/css">
+        
+    </head>
+    <body>
+        <div id="content">
+            <div id="nav">
+                <ul>
+                    <li><a href="../menu.php" title="Regresar"><img src="../img/baatras.png"></a></li>
+                    <li><a href="../menu.php" title="Inicio"><img src="../img/bahome.png"></a></li>
+                    <li><a href="contacto.php" title="Ayuda"><img src="../img/baayuda.png"></a></li>
+                    <li><a href="close.php" title="Salir"><img src="../img/baasalir.png"></a></li>
+                </ul>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a href='capa1.php'><img src="../img/cinstruc_1.png" class="grande"></a></li>
+                    <li><a href='Cursos2.php'><img src="../img/ccursos_1.png" class="grande"></a></li>
+                    <li><a href='.php'><img src="../img/coficios_2.png" class="grande"></a></li>
+                    <li><a href='manuales.php'><img src="../img/cmanuales_1.png" class="grande"></a></li>
+                    <li><a href='personal1.php'><img src="../img/cpersonal_1.png" class="grande"></a></li>
+                    <li><a href='periodoResumen.php'><img src="../img/CRP_1.png" class="grande" ></a></li>
+                    <li><a href='alta.php'><img src="../img/calta_1.png" class="grande" ></a></li>
+                   
+                </ul>
+            </div>
+        </div>
+     </body>
+</html>
